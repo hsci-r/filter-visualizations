7c89f521fd49ca4703075e63d63ac19c1936ec66
Maciej Janicki <maciej.janicki@helsinki.fi>
2021-11-30 17:11:24 +0200

    removed debug code

--- app.R
+++ app.R
@@ -3,9 +3,6 @@ library(shinyjs)
 library(tmap)
 library(yaml)
 
-message('DB_HOST: ', Sys.getenv('DB_HOST'))
-message('DB_USER: ', Sys.getenv('DB_USER'))
-
 options(shiny.sanitize.errors = FALSE, encoding='utf-8')
 
 shinyApp(ui = ui, server = server, enableBookmarking = "url")
